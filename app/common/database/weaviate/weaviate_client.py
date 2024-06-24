0d172d5df25b5a24cdab06f14c8aa5bffcb60b96
wishfyc <wishfyc@163.com>
2024-06-24 15:16:28 +0800

    code review

--- app/common/database/weaviate/weaviate_client.py
+++ app/common/database/weaviate/weaviate_client.py
@@ -6,5 +6,6 @@ class WeaviateClient:
     weaviate_dict = settings["weaviate"]
     client = weaviate.connect_to_local(
         host=weaviate_dict["host"],
+        port=weaviate_dict["port"],
         headers={"X-Huggingface-Api-Key": "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"}
     )
