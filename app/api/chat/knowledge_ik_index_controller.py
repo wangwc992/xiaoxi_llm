7e3ae0b03bc424400f84396c1cc2f934c2cba65d
wishfyc <wishfyc@163.com>
2024-06-25 09:45:52 +0800

    mysql

--- app/api/chat/knowledge_ik_index_controller.py
+++ app/api/chat/knowledge_ik_index_controller.py
@@ -76,35 +76,13 @@ async def stream_text(prompt_text: str) -> AsyncGenerator[bytes, None]:
     prompt_text = await generate_prompt(prompt_text)
     results_generator = engine.generate(prompt_text, sampling_params, request_id)
 
-    first_chunk_time = None
-    start_time = time.time()  # 记录开始时间
 
     async for request_output in results_generator:
-        if first_chunk_time is None:
-            first_chunk_time = time.time()  # 记录第一个流式输出的时间
-
         # 从 request_output 中提取所需的信息
         metrics = request_output.metrics
-
         text_outputs = [output.text for output in request_output.outputs]
         ret = {"text": text_outputs}
         yield (json.dumps(ret) + "\0").encode("utf-8")
-
-    end_time = time.time()  # 记录结束时间
-
-    # 格式化时间
-    start_time_formatted = datetime.fromtimestamp(start_time).strftime('%H:%M:%S')
-    first_chunk_time_formatted = datetime.fromtimestamp(first_chunk_time).strftime(
-        '%H:%M:%S') if first_chunk_time else 'N/A'
-    end_time_formatted = datetime.fromtimestamp(end_time).strftime('%H:%M:%S')
-
-    # 记录时间信息，可以是日志、文件或其他存储形式
-    print(f"Request started at {start_time_formatted}")
-    print(f"First chunk sent at {first_chunk_time_formatted}")
-    print(f"Request ended at {end_time_formatted}")
-
-    print(metrics)
-    print(type(metrics))
 
     # 格式化各个时间点
     arrival_time = datetime.fromtimestamp(metrics.arrival_time).strftime('%H:%M:%S')
@@ -131,6 +109,7 @@ async def stream_text(prompt_text: str) -> AsyncGenerator[bytes, None]:
         f"请求完成时间: {finished_time}, "
         f"从请求到达到处理完成的总时间: {total_duration:.3f} 秒"
     )
+    print(f"生成的文本: {text_outputs}")
 
 
 @router.post("/generate")
