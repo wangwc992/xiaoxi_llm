183eec66017b01a220c06e5a39cfdf71bb44c6be
wishfyc <wishfyc@163.com>
2024-07-19 13:34:25 +0800

    dev

--- app/api/knowledge_base/knowledge_base.py
+++ app/api/knowledge_base/knowledge_base.py
@@ -11,9 +11,9 @@ router = (APIRouter(prefix="/chat"))
 async def generate(request: ChatCompletionRequest,
                    raw_request: Request):
     """生成文本或流式返回生成的文本."""
-    request_dict = await request.json()
-    prompt = request_dict.pop("prompt")
-    stream = request_dict.pop("stream", False)
-    sampling_params = SamplingParams(**request_dict)
+    # request_dict = await request.json()
+    # prompt = request_dict.pop("prompt")
+    # stream = request_dict.pop("stream", False)
+    # sampling_params = SamplingParams(**request_dict)
 
     return create_chat_completion(request, raw_request)
