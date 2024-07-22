4c9dbb818bc5acc6f2cd523d62e44fa6a9cf1735
wishfyc <wishfyc@163.com>
2024-07-22 18:15:38 +0800

    完善 聊天一套

--- app/api/knowledge_base/knowledge_base.py
+++ app/api/knowledge_base/knowledge_base.py
@@ -1,17 +1,17 @@
 import os
 import json
 from typing import Optional, Union
-
+from datetime import datetime
 from fastapi import Request, APIRouter
 from fastapi.responses import JSONResponse, StreamingResponse
 from langchain_core.messages import SystemMessage
+from langfuse.client import Langfuse, ModelUsage
 from langfuse.decorators import observe, langfuse_context
-from langfuse.model import ModelUsage
 from pydantic import BaseModel
 from app.api.openai.api_server import create_chat_completion
 from app.common.utils.logging import get_logger
 from app.database.redis.redis_client import redis_client
-from vllm.entrypoints.openai.protocol import ChatCompletionRequest
+from vllm.entrypoints.openai.protocol import (ChatCompletionRequest, StreamOptions)
 
 from langchain_community.chat_message_histories import ChatMessageHistory
 
@@ -26,14 +26,13 @@ class MyChatCompletionRequestModel(BaseModel):
     model: str
 
 
-output = ''
-usage: Optional[Union[BaseModel, ModelUsage]] = None
-
-
-
 @router.post("/v1/chat/completions", response_model=None)
 async def generate(request: MyChatCompletionRequestModel, raw_request: Request):
     """生成文本或流式返回生成的文本."""
+    output = ''
+    usage: Optional[Union[BaseModel, ModelUsage]] = None
+    # 记录开始时间
+    start_time = datetime.now()
     # 获取请求头Authorization
     member_id = raw_request.headers.get("Authorization")
 
@@ -61,22 +60,48 @@ async def generate(request: MyChatCompletionRequestModel, raw_request: Request):
     # prompt = template.format(input=query, reference_data=reference_data)
 
     # 创建 ChatCompletionRequest 对象
-    request = ChatCompletionRequest(messages=message_list, stream=request.stream, model=request.model)
+    stream_options = StreamOptions(include_usage=True)
+    request = ChatCompletionRequest(messages=message_list, stream=request.stream, model=request.model,stream_options=stream_options)
     request.messages = message_list
 
     # 调用 create_chat_completion 并获取结果
     result = await create_chat_completion(request, raw_request)
+    # 记录结束时间
+    end_time = datetime.now()
+
+    loger.info(f"start_time: {start_time}, end_time: {end_time}")
     # 保存消息到聊天历史记录
-    await save_message(chat_message_history, chat_message_history_key, result)
+    message_dict = await extract_message(result)
+
+    #     # 添加 AI 消息到聊天历史记录
+    # chat_message_history.add_ai_message(message_dict.get('output'))
+    # # 聊天历史记录保存到 Redis
+    # redis_client.set_object(chat_message_history_key, chat_message_history)
+
+    # 使用 Langfuse 保存消息
+    await save_langfuse(member_id, message_list, message_dict.get('output'), message_dict.get('usage'), start_time,
+                        end_time)
     # 返回结果
     return result
 
+
 @observe()
-async def save_message(chat_message_history, chat_message_history_key, result):
+async def save_langfuse(member_id, message_list, output, usage, start_time, end_time):
     '''保存消息到聊天历史记录'''
-    global output
-    global usage
-    result_content = ''
+    langfuse_context.update_current_observation(
+        user_id=member_id,
+        metadata={"test": "test value"},
+        input=message_list,
+        output=output,
+    )
+    trace_id = langfuse_context.get_current_trace_id()
+    Langfuse().generation(usage=usage, trace_id=trace_id, start_time=start_time, end_time=end_time)
+
+
+async def extract_message(result):
+    '''保存消息到聊天历史记录'''
+    output = ''
+    usage = None
     # 处理 JSONResponse
     if isinstance(result, JSONResponse):
         result_body = result.body
@@ -87,10 +112,9 @@ async def save_message(chat_message_history, chat_message_history_key, result):
         if usage_or:
             # {'prompt_tokens': 31, 'total_tokens': 472, 'completion_tokens': 441}
             usage = ModelUsage(input=usage_or.get('prompt_tokens'), output=usage_or.get('completion_tokens'),
-                               total=usage_or.get('total_tokens'))
+                               total=usage_or.get('total_tokens'), unit='TOKENS')
             loger.info(f"usage: {usage}")
 
-    # 处理 StreamingResponse
     # 处理 StreamingResponse
     elif isinstance(result, StreamingResponse):
         async for chunk in result.body_iterator:
@@ -104,22 +128,21 @@ async def save_message(chat_message_history, chat_message_history_key, result):
             # 解析 JSON 数据
             try:
                 chunk_data = json.loads(chunk)
-                if chunk_data.get('choices')[0].get('finish_reason') != 'stop':
+                choices = chunk_data.get('choices')
+                if choices and choices[0].get('finish_reason') != 'stop':
                     delta_content = chunk_data.get('choices')[0].get('delta').get('content')
                     if delta_content:  # 检查 content 是否为 None
                         output += delta_content
-
+                else:
+                    usage_or = chunk_data.get('usage')
+                    if usage_or:
+                        usage = ModelUsage(input=usage_or.get('prompt_tokens'),
+                                           output=usage_or.get('completion_tokens'),
+                                           total=usage_or.get('total_tokens'), unit='TOKENS')
+                        loger.info(f"usage: {usage}")
             except json.JSONDecodeError as e:
                 loger.error(f"JSONDecodeError: {e} - Skipping chunk: {chunk}")
-
-    langfuse_context.update_current_observation(
-        user_id="1234",
-        metadata={"test": "test value"},
-        input=chat_message_history.messages,
-        output=result_content,
-        usage=usage
-    )
-    #     # 添加 AI 消息到聊天历史记录
-    # chat_message_history.add_ai_message(content)
-    # # 聊天历史记录保存到 Redis
-    # redis_client.set_object(chat_message_history_key, chat_message_history)
+    return {
+        "output": output,
+        "usage": usage
+    }
