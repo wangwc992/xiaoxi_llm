f2d2879b3383063d25f53abd1a2371e0e9102246
wishfyc <wishfyc@163.com>
2024-08-02 18:29:17 +0800

    使用 SentenceTransformer

--- app/common/core/langchain_client.py
+++ app/common/core/langchain_client.py
@@ -10,26 +10,26 @@ from app.common.core.config import settings
 
 class Embedding:
     embedding_arg = settings["embedding"]
-    # model_kwargs = {'device': embedding_arg["device"]}
+    model_kwargs = {'device': embedding_arg["device"]}
     encode_kwargs = {'normalize_embeddings': False}
     print(f"Embedding: {embedding_arg['embedding_path']}，device: {embedding_arg['device']}，normalize_embeddings: {False}")
-    # embedding = HuggingFaceEmbeddings(
-    #     model_name=embedding_arg['embedding_path'],
-    #     # model_kwargs=model_kwargs,
-    #     encode_kwargs=encode_kwargs
-    # )
-    model = SentenceTransformer(embedding_arg['embedding_path'])
+    embedding = HuggingFaceEmbeddings(
+        model_name=embedding_arg['embedding_path'],
+        model_kwargs=model_kwargs,
+        encode_kwargs=encode_kwargs
+    )
+    # model = SentenceTransformer(embedding_arg['embedding_path'])
 
 
     @classmethod
     def embed_query(cls, text: str) -> List[float]:
-        # return cls.embedding.embed_query(text)
-        return cls.model.encode(text)[0].tolist()
+        return cls.embedding.embed_query(text)
+        # return cls.model.encode(text)[0].tolist()
 
     @classmethod
     def embed_documents(cls, texts: List[str]) -> List[List[float]]:
-        # return cls.embedding.embed_documents(texts)
-        return cls.model.encode(text).tolist()
+        return cls.embedding.embed_documents(texts)
+        # return cls.model.encode(text).tolist()
 
 
 def get_gpu_count():
