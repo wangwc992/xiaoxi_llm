7df670d5552d911feb36030a7ce33d77435908e3
wishfyc <wishfyc@163.com>
2024-08-02 11:37:10 +0800

    匹配 vllm 写法

--- app/common/core/langchain_client.py
+++ app/common/core/langchain_client.py
@@ -1,15 +1,9 @@
 import subprocess
 
-import torch
 from langchain_huggingface import HuggingFaceEmbeddings
 from typing import List
 
 from app.common.core.config import settings
-from vllm.engine.arg_utils import AsyncEngineArgs
-from vllm.engine.async_llm_engine import AsyncLLMEngine
-from vllm.usage.usage_lib import UsageContext
-from vllm.entrypoints.openai.serving_chat import OpenAIServingChat
-from vllm.entrypoints.openai.serving_completion import OpenAIServingCompletion
 
 
 class Embedding:
@@ -42,65 +36,3 @@ def get_gpu_count():
     else:
         print(f"Error executing nvidia-smi: {result.stderr}")
         return 0
-
-
-class VllmClient:
-    # 通过 nvidia-smi -L 指令获取 GPU 个数
-    # 执行 nvidia-smi -L 命令并捕获输出
-    vllm = settings.get('vllm')
-    gpu_count = get_gpu_count()
-    if gpu_count >= 4:
-        engine_args = vllm.get('qwen2_72B_instruct_gptq_int4')
-    else:
-        engine_args = vllm.get('qwen2_7B_instruct')
-    async_engineArgs = AsyncEngineArgs(**engine_args)
-    served_model_names = engine_args.get('model')
-    model_config = None
-    openai_serving_chat = None
-    openai_serving_completion = None
-    response_role = "1"
-
-    engine = AsyncLLMEngine.from_engine_args(async_engineArgs, usage_context=UsageContext.API_SERVER)
-
-    @classmethod
-    async def initialize(cls):
-        cls.model_config = await cls.engine.get_model_config()
-        cls.openai_serving_chat = OpenAIServingChat(
-            cls.engine,
-            cls.model_config,
-            cls.served_model_names,
-            cls.response_role,
-            lora_modules=None,
-            prompt_adapters=None,
-            request_logger=None,
-            chat_template=None,
-        )
-        cls.openai_serving_completion = OpenAIServingCompletion(
-            cls.engine,
-            cls.model_config,
-            cls.served_model_names,
-            lora_modules=None,
-            prompt_adapters=None,
-            request_logger=None)
-
-    @classmethod
-    def get_openai_serving_chat(cls):
-        return cls.openai_serving_chat
-
-    @classmethod
-    def get_openai_serving_completion(cls):
-        return cls.openai_serving_completion
-
-
-# Ensure async initialization is called properly in the main application
-async def initialize_vllm_client():
-    await VllmClient.initialize()
-
-
-# These need to be awaited in an async context
-def get_openai_serving_chat():
-    return VllmClient.get_openai_serving_chat()
-
-
-def get_openai_serving_completion():
-    return VllmClient.get_openai_serving_completion()
