d33d0f14e8199e6ec22eaef5b3ec9bb6e447741e
wishfyc <wishfyc@163.com>
2024-08-02 15:25:02 +0800

    注释 HuggingFaceEmbeddings

--- app/database/weaviate/weaviate_client.py
+++ app/database/weaviate/weaviate_client.py
@@ -23,17 +23,23 @@ class WeaviateClient:
             headers={"X-Huggingface-Api-Key": "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"}
         )
     else:
-        client = weaviate.WeaviateClient(
-            embedded_options=EmbeddedOptions(
-                binary_path="/root/autodl-tmp/database/w2",
-                additional_env_vars={
-                    # "ENABLE_MODULES": "text2vec-transformers",
-                    # "TRANSFORMERS_INFERENCE_API": 'http://127.0.0.1:8090',
-                    "BACKUP_FILESYSTEM_PATH": "/root/autodl-tmp/database/w2"
-                }
-            )
+        # 先连接到本地的Weaviate数据库，本地不存在在weaviate.WeaviateClient
+        client = weaviate.connect_to_local(
+            port=8079,
+            grpc_port=50060,
         )
-        client.connect()
+        if not client.is_ready():
+            client = weaviate.WeaviateClient(
+                embedded_options=EmbeddedOptions(
+                    binary_path="/root/autodl-tmp/database/w2",
+                    additional_env_vars={
+                        # "ENABLE_MODULES": "text2vec-transformers",
+                        # "TRANSFORMERS_INFERENCE_API": 'http://127.0.0.1:8090',
+                        "BACKUP_FILESYSTEM_PATH": "/root/autodl-tmp/database/w2"
+                    }
+                )
+            )
+            client.connect()
 
     def __init__(self, collections_name):
         self.collections_name = collections_name
