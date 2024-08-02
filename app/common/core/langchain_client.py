5d670a2589deb800ab5cef37fee76f154c010edb
wishfyc <wishfyc@163.com>
2024-08-02 15:48:16 +0800

    注释 HuggingFaceEmbeddings

--- app/common/core/langchain_client.py
+++ app/common/core/langchain_client.py
@@ -8,12 +8,12 @@ from app.common.core.config import settings
 
 class Embedding:
     embedding_arg = settings["embedding"]
-    model_kwargs = {'device': embedding_arg["device"]}
+    # model_kwargs = {'device': embedding_arg["device"]}
     encode_kwargs = {'normalize_embeddings': False}
     print(f"Embedding: {embedding_arg['embedding_path']}，device: {embedding_arg['device']}，normalize_embeddings: {False}")
     embedding = HuggingFaceEmbeddings(
         model_name=embedding_arg['embedding_path'],
-        model_kwargs=model_kwargs,
+        # model_kwargs=model_kwargs,
         encode_kwargs=encode_kwargs
     )
 
