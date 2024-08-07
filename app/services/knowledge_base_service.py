34f5fd0741b863bd2fb9d51b6f076c70aaba6d5b
wishfyc <wishfyc@163.com>
2024-08-07 11:27:53 +0800

    添加 serving_chat.py

--- app/services/knowledge_base_service.py
+++ app/services/knowledge_base_service.py
@@ -97,7 +97,7 @@ async def stream_response(result, chat_message_history, chat_message_history_key
     usage = None
     first_chunk = True
     async for chunk in result.body_iterator:
-        if raw_request.is_disconnected():
+        if raw_request is not None and await raw_request.is_disconnected():
             logger.info("************************************,输出已经断开")
         logger.info(f"chunk: {chunk}")
         if first_chunk:
