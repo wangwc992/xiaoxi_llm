eae6a2cd30323aab9d97a5703326b705919e4b42
wishfyc <wishfyc@163.com>
2024-08-08 16:12:09 +0800

    code review

--- app/data/dictionaries/__init__.py
+++ app/data/dictionaries/__init__.py
@@ -26,18 +26,15 @@ def load_dictionaries(words, filepath):
 # README.md
 # scu_stopwords.txt
 # stopwords.txt
-load_dictionaries(stopword_words, './stopwords/scu_stopwords.txt')
-load_dictionaries(stopword_words, './stopwords/cn_stopwords.txt')
-load_dictionaries(stopword_words, './stopwords/hit_stopwords.txt')
-load_dictionaries(stopword_words, './stopwords/scu_stopwords.txt')
-load_dictionaries(stopword_words, './stopwords/baidu_stopwords.txt')
-load_dictionaries(stopword_words, './stopwords/english')
-load_dictionaries(stopword_words, './stopwords/chinese')
-
-
-
-load_dictionaries(sensitive_words, './sensitive/广告.txt')
-load_dictionaries(sensitive_words, './sensitive/政治类.txt')
-load_dictionaries(sensitive_words, './sensitive/涉枪涉爆违法信息关键词.txt')
-load_dictionaries(sensitive_words, './sensitive/网址.txt')
-load_dictionaries(sensitive_words, './sensitive/色情类.txt')
+load_dictionaries(stopword_words, _get_abs_path('stopwords/scu_stopwords.txt'))
+load_dictionaries(stopword_words, _get_abs_path('stopwords/cn_stopwords.txt'))
+load_dictionaries(stopword_words, _get_abs_path('stopwords/hit_stopwords.txt'))
+load_dictionaries(stopword_words, _get_abs_path('stopwords/scu_stopwords.txt'))
+load_dictionaries(stopword_words, _get_abs_path('stopwords/baidu_stopwords.txt'))
+load_dictionaries(stopword_words, _get_abs_path('stopwords/english'))
+load_dictionaries(stopword_words, _get_abs_path('stopwords/chinese'))
+load_dictionaries(sensitive_words, _get_abs_path('sensitive/广告.txt'))
+load_dictionaries(sensitive_words, _get_abs_path('sensitive/政治类.txt'))
+load_dictionaries(sensitive_words, _get_abs_path('sensitive/涉枪涉爆违法信息关键词.txt'))
+load_dictionaries(sensitive_words, _get_abs_path('sensitive/网址.txt'))
+load_dictionaries(sensitive_words, _get_abs_path('sensitive/色情类.txt'))
