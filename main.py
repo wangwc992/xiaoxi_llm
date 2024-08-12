f7a9ff1e6070598aea36bb448eb94ba459976777
wishfyc <wishfyc@163.com>
2024-08-12 11:03:22 +0800

    yjk库洗数据

--- main.py
+++ main.py
@@ -91,12 +91,12 @@ def build_app(args, **uvicorn_kwargs):
         # 检查是否包含敏感词
         for word in sensitive_words:
             if word in body_text:
-                result = '''data: {"choices": [
+                result = f'''data: {"choices": [
                     {
                         "index": 0,
                         "delta": {
                             "role": "3",
-                            "content": "您的问题涉及敏感内容，小希无法回答呦~请换个话题吧~"
+                            "content": "您的问题涉及敏感内容 {word}，小希无法回答呦~请换个话题吧~"
                         },
                     }
                 ]}'''
