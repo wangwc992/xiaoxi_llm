6d6b5f54e858b77d1c6a30b79faa8147fbcf1933
wishfyc <wishfyc@163.com>
2024-08-15 09:33:18 +0800

    eval 转换字典数据有换行的错误

--- app/common/core/config.py
+++ app/common/core/config.py
@@ -6,6 +6,7 @@ import subprocess
 # Load environment variables from .env file
 load_dotenv()
 
+
 def load_config():
     app_env = os.getenv('APP_ENV', 'dev')
     base_dir = os.path.dirname(os.path.abspath(__file__))
@@ -15,19 +16,20 @@ def load_config():
         config = yaml.safe_load(f)
     return config
 
+
 settings = load_config()
 
 
-# def get_gpu_count():
-#     result = subprocess.run(['nvidia-smi', '-L'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-#
-#     if result.returncode == 0:
-#         # 按行分割输出并计算行数，行数即为 GPU 的数量
-#         gpu_lines = result.stdout.strip().split('\n')
-#         return len(gpu_lines)
-#     else:
-#         print(f"Error executing nvidia-smi: {result.stderr}")
-#         return 0
-#
-#
-# settings['gpu_count'] = get_gpu_count()
+def get_gpu_count():
+    result = subprocess.run(['nvidia-smi', '-L'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+    if result.returncode == 0:
+        # 按行分割输出并计算行数，行数即为 GPU 的数量
+        gpu_lines = result.stdout.strip().split('\n')
+        return len(gpu_lines)
+    else:
+        print(f"Error executing nvidia-smi: {result.stderr}")
+        return 0
+
+
+settings['gpu_count'] = get_gpu_count()
