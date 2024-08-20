124f2eb5b3cce95fcb9aed0e8382b4c3bc1269f3
wishfyc <wishfyc@163.com>
2024-08-20 10:24:42 +0800

    知识库 binglog

--- app/start_init/mysql_binglog_monitoring.py
+++ app/start_init/mysql_binglog_monitoring.py
@@ -64,7 +64,7 @@ async def sync_t_knowledge_info(data: dict):
 async def start_binlog_listener():
     # MySQL 连接配置
     mysql_settings = settings["mysql"]["xxlxdb"]
-    database = mysql_settings.get('database')
+    database_name = mysql_settings.get('database')
     mysql_settings.pop('database')
 
     # 获取指定表的字段名
@@ -83,7 +83,7 @@ async def start_binlog_listener():
         connection_settings=mysql_settings,
         server_id=1,  # 随便设置一个唯一的 server_id
         blocking=True,
-        only_schemas=[database],  # 监听多个数据库
+        only_schemas=[database_name],  # 监听多个数据库
         only_tables=["t_knowledge_info"],  # 监听多个表
         resume_stream=True,
     )
