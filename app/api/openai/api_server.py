192f136a49a2df7116052dba391f146a1c4d8140
wishfyc <wishfyc@163.com>
2024-08-20 15:10:13 +0800

    知识库 binglog

--- app/api/openai/api_server.py
+++ app/api/openai/api_server.py
@@ -158,7 +158,7 @@ async def create_embedding(request: EmbeddingRequest, raw_request: Request):
 
 @router.get("/chat/abort", description="Abort the request with the given ID.")
 async def abort(request_id: str):
-    openai_serving_chat.engine.abort(request_id)
+    await openai_serving_chat.engine.abort(request_id)
     return JSONResponse(content={"message": "Request aborted."})
 
 
