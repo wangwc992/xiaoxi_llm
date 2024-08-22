69db3df9d9755531bb70f7a6b67866990429fdf6
wishfyc <wishfyc@163.com>
2024-08-22 19:00:13 +0800

    测试 num_running_sys

--- app/api/openai/api_server.py
+++ app/api/openai/api_server.py
@@ -225,6 +225,6 @@ async def build_server(
 
 async def scheduler():
     global engine
-    stats = await engine._get_stats(None, None)
-    print(stats.num_running_sys)
-    print(stats.__dict__)
+    stats =  await engine.do_log_stats()
+    # print(stats.num_running_sys)
+    # print(stats.__dict__)
