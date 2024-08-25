fe998fb7cff36de40701344635984a6722857dca
wishfyc <wishfyc@163.com>
2024-08-25 22:21:30 +0800

    开启监听

--- app/start_init/timed_task.py
+++ app/start_init/timed_task.py
@@ -3,8 +3,10 @@ import time
 
 from app.common.utils.logging import get_logger
 from app.data_cleansing.knowledge_base_cleansing import cleansing_manner_execution, MannerExecution
+from app.database.weaviate.knowledge_base import knowledge_base_weaviate
 
 logger = get_logger(__name__)
+
 
 def run_scheduler():
     schedule.every().day.at("03:00").do(knowledge_base_cleansing)
@@ -14,8 +16,14 @@ def run_scheduler():
 
 
 async def knowledge_base_cleansing():
-    logger.info("开始清洗知识库数据")
-#     每天凌晨三点清洗一次
+    logger.info("清空历史数据")
+    method_lsit = ['platform_introduction', 'zn_school_info', 'zn_school_info_rank', 'zn_school_info_more',
+                   'zn_school_selection_reason', 'zn_school_admission_undergraduate',
+                   'zn_school_admission_graduate_student', 'zn_school_admission_art', 'zn_school_department_project']
+    for method_name in method_lsit:
+        result = knowledge_base_weaviate.clear_all_data(property="db_name", like_str=method_name)
+        logger.info(f"清空{method_name}数据结果: {result}")
+    logger.info("开始清洗数据")
     manner_execution = MannerExecution(
         method_name="t_knowledge_info",
         limit=1000,
@@ -24,4 +32,3 @@ async def knowledge_base_cleansing():
         is_async=True
     )
     await cleansing_manner_execution(manner_execution)
-
