10122bc0319e1c53fd6630949bc6cb0762938adf
wishfyc <wishfyc@163.com>
2024-08-25 19:36:38 +0800

    测试 search_knowledge_info_data2

--- app/data_cleansing/knowledge_base_cleansing.py
+++ app/data_cleansing/knowledge_base_cleansing.py
@@ -836,7 +836,7 @@ async def cleansing_manner_execution(manner_execution: MannerExecution):
                         logger.info(f"{method_name} 数据清洗完成")
                         break
                     uuid_list = insert_weaviate_data_all(knowledge_base_model)
-                    # insert_mysql_weaviate(knowledge_base_model, uuid_list, file_url_list)
+                    insert_mysql_weaviate(knowledge_base_model, uuid_list, file_url_list)
                     frequency -= 1
             except Exception as e:
                 logger.error(e)
