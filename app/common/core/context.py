cbc818557fc8be21c99a74350a95daba4e6fe4bf
wishfyc <wishfyc@163.com>
2024-08-26 10:39:32 +0800

    code review

--- app/common/core/context.py
+++ app/common/core/context.py
@@ -14,9 +14,8 @@ chat_visits_number_max = 30
 
 
 # 用于记录chat接口的请求次数，防止请求次数超过上限
-# 在源码/root/miniconda3/envs/agiclass/lib/python3.10/site-packages/vllm/engine/async_llm_engine.py
-# 143：process_request_output 、184：abort_request
-# 用到了这个函数
+# /root/miniconda3/envs/agiclass/lib/python3.10/site-packages/vllm/engine/llm_engine.py
+#  1094：context.chat_visits_number = num_running_sys
 def get_chat_visits_number() -> bool:
     global chat_visits_number
     global chat_visits_number_max
--- app/test/test.py
+++ app/test/test.py
@@ -1,16 +1,25 @@
+import asyncio
+
 from fastapi import FastAPI, BackgroundTasks
 import schedule
 import time
 import threading
 
+from app.common.utils.logging import get_logger
+
+logger = get_logger(__name__)
 app = FastAPI()
 
 
 async def job():
     print("任务执行中...")
-
+def sync_job():
+    asyncio.run(job())
 
 def run_scheduler():
+    logger.info("启动定时任务")
+    # 设置每分钟 第三秒执行一次任务
+    schedule.every().minute.at(":03").do(sync_job)
     while True:
         schedule.run_pending()
         time.sleep(1)
@@ -18,9 +27,6 @@ def run_scheduler():
 
 @app.on_event("startup")
 def start_scheduler():
-    # 设置每分钟 第三秒执行一次任务
-    schedule.every().minute.at(":03").do(job)
-
     # 启动一个后台线程来运行调度器
     scheduler_thread = threading.Thread(target=run_scheduler)
     scheduler_thread.daemon = True  # 守护线程，主线程退出时也随之退出
