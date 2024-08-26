c881ef6462ec69e6829ab277f9609d3af6d9a896
wishfyc <wishfyc@163.com>
2024-08-26 10:11:08 +0800

    测试定时任务

--- app/start_init/timed_task.py
+++ app/start_init/timed_task.py
@@ -9,7 +9,9 @@ logger = get_logger(__name__)
 
 
 def run_scheduler():
-    schedule.every().day.at("03:00").do(knowledge_base_cleansing)
+    #每分钟 第三秒执行一次任务
+    schedule.every().minute.at(":03").do(knowledge_base_cleansing)
+    # schedule.every().day.at("03:00").do(knowledge_base_cleansing)
     while True:
         schedule.run_pending()
         time.sleep(1)
@@ -17,18 +19,18 @@ def run_scheduler():
 
 async def knowledge_base_cleansing():
     logger.info("清空历史数据")
-    method_lsit = ['platform_introduction', 'zn_school_info', 'zn_school_info_rank', 'zn_school_info_more',
-                   'zn_school_selection_reason', 'zn_school_admission_undergraduate',
-                   'zn_school_admission_graduate_student', 'zn_school_admission_art', 'zn_school_department_project']
-    for method_name in method_lsit:
-        result = knowledge_base_weaviate.clear_all_data(property="db_name", like_str=method_name)
-        logger.info(f"清空{method_name}数据结果: {result}")
-    logger.info("开始清洗数据")
-    manner_execution = MannerExecution(
-        method_name="t_knowledge_info",
-        limit=1000,
-        start_id=0,
-        frequency=-1,
-        is_async=True
-    )
-    await cleansing_manner_execution(manner_execution)
+    # method_lsit = ['platform_introduction', 'zn_school_info', 'zn_school_info_rank', 'zn_school_info_more',
+    #                'zn_school_selection_reason', 'zn_school_admission_undergraduate',
+    #                'zn_school_admission_graduate_student', 'zn_school_admission_art', 'zn_school_department_project']
+    # for method_name in method_lsit:
+    #     result = knowledge_base_weaviate.clear_all_data(property="db_name", like_str=method_name)
+    #     logger.info(f"清空{method_name}数据结果: {result}")
+    # logger.info("开始清洗数据")
+    # manner_execution = MannerExecution(
+    #     method_name="t_knowledge_info",
+    #     limit=1000,
+    #     start_id=0,
+    #     frequency=-1,
+    #     is_async=True
+    # )
+    # await cleansing_manner_execution(manner_execution)
