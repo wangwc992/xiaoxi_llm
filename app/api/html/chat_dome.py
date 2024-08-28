ef857a4ec0524a3ee669f2a3302aa19d8e38a524
wishfyc <wishfyc@163.com>
2024-08-28 09:12:11 +0800

    加载html页面

---
+++ app/api/html/chat_dome.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from starlette.requests import Request
+
+router = APIRouter(prefix="/html", tags=["html"])
+
+# 设置模板目录
+templates = Jinja2Templates(directory="templates")
+
+
+@router.get("/chat_dome", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("chat_dome.html", {"request": request})
