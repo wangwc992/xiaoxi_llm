2f4c3660eac3f1186c4908abef8c5d3505a61424
wishfyc <wishfyc@163.com>
2024-08-29 10:28:12 +0800

    任务C返回

--- app/services/knowledge_base_service.py
+++ app/services/knowledge_base_service.py
@@ -134,6 +134,9 @@ async def knowledge_base_generate(request: MyChatCompletionRequestModel, raw_req
                 result = await create_chat_completion(chat_request, raw_request)
                 result_dict = await extract_message(result)
                 result_dict.pop("usage")
+                output = result_dict.get("output")
+                # 字符串转换为字典
+                result_dict["output"] = eval(output)
                 result_dict["classification"] = classification_model.dict()
                 return JSONResponse(content=result_dict)
     else:
