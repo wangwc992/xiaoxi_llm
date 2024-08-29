857164c4572c747accb33ca112f20534015c8b0e
wishfyc <wishfyc@163.com>
2024-08-29 10:15:09 +0800

    任务C返回

--- app/services/knowledge_base_service.py
+++ app/services/knowledge_base_service.py
@@ -47,7 +47,10 @@ class ClassificationModel(BaseModel):
     school_name: Optional[str] = Field(None, description="学校名称")
     major_name: Optional[str] = Field(None, description="专业名称")
 
-result_format = '''data: {"choices": [ { "index": 0, "delta": { "role": "s%", "content": "s%" }} ]}'''
+
+result_format = '''{"choices": [ { "index": 0, "delta": { "role": "%s", "content": "%s" }} ]}'''
+
+
 async def knowledge_base_generate(request: MyChatCompletionRequestModel, raw_request: Request,
                                   background_tasks: BackgroundTasks):
     # 接口开始时间
@@ -97,7 +100,7 @@ async def knowledge_base_generate(request: MyChatCompletionRequestModel, raw_req
     elif query_type == "C":
         # 小希平台进行留学申请相关操作
         student_name = classification_model.student_name
-        result = str.format(result_format, "1", "学生姓名为空")
+        result = result_format % ("5", "学生姓名为空")
         if not student_name:
             return result
         else:
@@ -105,7 +108,7 @@ async def knowledge_base_generate(request: MyChatCompletionRequestModel, raw_req
             if task == "11":
                 application_progress_data_list = await get_application_progress_data_list(student_name)
                 if not application_progress_data_list:
-                    result = str.format(result_format, "1", "学生申请信息不存在")
+                    result = result_format % ("5", "学生姓名为空")
                     return result
                 # 加载prompt模板
                 template = PromptTemplate.from_template(matching_summary)
@@ -115,7 +118,7 @@ async def knowledge_base_generate(request: MyChatCompletionRequestModel, raw_req
                 # 加载prompt模板
                 service_school_dict_list = select_service_school(student_name)
                 if not service_school_dict_list:
-                    result = str.format(result_format, "1", "学生申请信息不存在")
+                    result = result_format % ("5", "学生姓名为空")
                     return result
                 template = PromptTemplate.from_template(matching_information)
                 prompt = template.format(input=query, student_info=classification_model,
--- app/test/test.py
+++ app/test/test.py
@@ -1,20 +1,3 @@
-import time
-from fastapi import FastAPI
-from starlette.responses import StreamingResponse
-
-app = FastAPI()
-
-async def get_data():
-    yield '''data: {"choices": [ { "index": 0, "delta": { "role": "2", "content": "chat线程数量超了" }} ]}'''
-    yield '''data: {"choices": [ { "index": 0, "delta": { "role": "2", "content": "chat线程数量超了" }} ]}'''
-    time.sleep(5)
-    yield "3"
-    yield "4"
-
-@app.get("/")
-async def read_root():
-    yield StreamingResponse(get_data(), media_type="text/event-stream")
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+result_format = '''data: {"choices": [ { "index": 0, "delta": { "role": "%s", "content": "%s" }} ]}'''
+result = result_format % ("1", "学生姓名为空")
+print(result)
