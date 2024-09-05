ec6de4b234e935b4e6feebb0f9547aa53f4ea89c
wishfyc <wishfyc@163.com>
2024-09-05 11:24:30 +0800

    创建一个调用http接口的py

--- app/services/knowledge_base_service.py
+++ app/services/knowledge_base_service.py
@@ -494,7 +494,7 @@ async def reference_networked_rag(query: str):
 
     networked_reference_datas = []
     for text in text_list:
-        generator = get_tokens(text)
+        generator = await get_tokens(text)
         logger.info(f"*****************generator: {generator},type: {type(generator)}")
         tokens = generator.get("tokens")
         count = generator.get("count")
