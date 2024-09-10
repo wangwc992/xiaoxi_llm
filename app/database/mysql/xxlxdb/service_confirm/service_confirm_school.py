b89ee81c51d8db26b5442a2a1f9cc328f4faa929
wishfyc <wishfyc@163.com>
2024-09-10 13:38:39 +0800

    纯中文没找到 拼音找学生

--- app/database/mysql/xxlxdb/service_confirm/service_confirm_school.py
+++ app/database/mysql/xxlxdb/service_confirm/service_confirm_school.py
@@ -61,7 +61,6 @@ def select_student_by_member_id(member_id_list: list, student_name: str):
     member_id_str = "','".join(member_id)
     sql = f"SELECT * from service_master where adviser_member_id in ('{member_id_str}') and user_real_name = '{student_name}'"
     student_dict_list = xxlxdb.execute_all2dict(sql)
-    student_list = [student_dict['user_real_name'] for student_dict in student_dict_list]
     return student_list
 
 
