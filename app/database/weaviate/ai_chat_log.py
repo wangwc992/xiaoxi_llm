bdd9fdf12e575b9d01922ee0b51c68b9f7934b7d
wishfyc <wishfyc@163.com>
2024-09-11 17:39:15 +0800

    optimize

--- app/database/weaviate/ai_chat_log.py
+++ app/database/weaviate/ai_chat_log.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from langchain_core.pydantic_v1 import BaseModel, Field
+from pydantic import BaseModel, Field
 from weaviate.classes.config import Configure, Property, DataType
 from weaviate.collections.classes.grpc import HybridFusion, MetadataQuery
 
--- app/services/knowledge_base_service.py
+++ app/services/knowledge_base_service.py
@@ -457,7 +457,7 @@ async def save_weaviste(ai_chat_log_model: AiChatLogModel):
         created_time=ai_chat_log_model.start_time,
     ),
     vector = Embedding.embed_query(ai_chat_log_model.output)
-    uuid = ai_chat_log_weaviate.insert_data(ai_chat_log_model.dict(), vector)
+    uuid = ai_chat_log_weaviate.insert_data(ai_chat_weaviate_model.__dict__, vector)
 
 
 async def save_redis(chat_message_history, chat_message_history_key, result_dict):
