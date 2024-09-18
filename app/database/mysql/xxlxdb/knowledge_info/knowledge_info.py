9955d3199525975718a7dca4921787f14a24d588
wishfyc <wishfyc@163.com>
2024-09-18 09:04:58 +0800

    发版

--- app/database/mysql/xxlxdb/knowledge_info/knowledge_info.py
+++ app/database/mysql/xxlxdb/knowledge_info/knowledge_info.py
@@ -34,7 +34,7 @@ from datetime import datetime
 from typing import Optional
 from langchain_core.pydantic_v1 import BaseModel, Field
 
-from app.database.mysql.mysql_client import xxlxdb, smart_counselor, yhj
+from app.database.mysql.mysql_client import xxlxdb, smart_counselor
 
 
 class KnowledgeInfo(BaseModel):
@@ -77,9 +77,9 @@ def search_knowledge_info_data(id=0, limit=10):
     return xxlxdb.execute_all2dict(sql=sql, limit=limit)
 
 
-def search_knowledge_info_data2(id=0, limit=10):
-    sql = f"SELECT id, type, country, school, class, name, founder,filename, replyerTime, content, fileurl,attachment_content FROM weaviate_knowledge_info where id > {id}"
-    return yhj.execute_all2dict(sql=sql, limit=limit)
+# def search_knowledge_info_data2(id=0, limit=10):
+#     sql = f"SELECT id, type, country, school, class, name, founder,filename, replyerTime, content, fileurl,attachment_content FROM weaviate_knowledge_info where id > {id}"
+#     return yhj.execute_all2dict(sql=sql, limit=limit)
 
 
 def search_notice_message_data(id: int = 0, limit: int = 10):
