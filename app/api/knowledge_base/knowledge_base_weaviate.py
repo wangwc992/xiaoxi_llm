a0d3dd61d65d198bec36df4ceafea4ee2b35ad67
wishfyc <wishfyc@163.com>
2024-09-27 10:43:03 +0800

    knowledge_base_weaviate.py 代码结构调整

--- app/api/knowledge_base/knowledge_base_weaviate.py
+++ app/api/knowledge_base/knowledge_base_weaviate.py
@@ -10,6 +10,18 @@ router = APIRouter(prefix="/knowledge_base/weaviate")
 logger = get_logger(__name__)
 
 
+@router.post("/collection", description="Create a weaviate collection")
+async def create_or_del(create_or_del: bool):
+    """ 创建或删除weaviate的collection
+    :param create_or_del: true为创建，false为删除
+    """
+    if create_or_del:
+        result = knowledge_base_weaviate.create_collection(knowledge_base_weaviate.properties)
+    else:
+        result = knowledge_base_weaviate.delete_collection_name(knowledge_base_weaviate.collections_name)
+    return result
+
+
 @router.post("/cleansing", description="Cleansing the knowledge base.")
 async def cleansing(manner_execution: MannerExecution):
     """知识库数据清洗
@@ -31,28 +43,6 @@ async def cleansing(manner_execution: MannerExecution):
     return await cleansing_manner_execution(manner_execution)
 
 
-@router.get("/weaviateSearch")
-async def reference_data(query: str, limit: int = 10):
-    '''RAG结果引用数据查询
-    query: str   查询字符串
-    alpha: float   阈值，alpha 为 1 是纯向量搜索，alpha 为 0 是纯文本搜索
-    limit: int   限制数量
-    '''
-    return await knowledge_base_weaviate.search_hybrid(query=query,limit=limit)
-
-
-@router.post("/collection", description="Create a weaviate collection")
-async def create_or_del(create_or_del: bool):
-    """ 创建或删除weaviate的collection
-    :param create_or_del: true为创建，false为删除
-    """
-    if create_or_del:
-        result = knowledge_base_weaviate.create_collection(knowledge_base_weaviate.properties)
-    else:
-        result = knowledge_base_weaviate.delete_collection_name(knowledge_base_weaviate.collections_name)
-    return result
-
-
 @router.delete("/clearDatabaseData", description="Clear all data in weaviate")
 async def clear_all_data(like_str: str, ):
     """ 清空weaviate的数据,根据db_name,
@@ -61,6 +51,25 @@ async def clear_all_data(like_str: str, ):
     """
     result = knowledge_base_weaviate.clear_all_data(property="db_name", like_str=like_str)
     return result
+
+
+@router.get("/weaviateSearch")
+async def reference_data(query: str, limit: int = 10):
+    '''RAG结果引用数据查询
+    query: str   查询字符串
+    alpha: float   阈值，alpha 为 1 是纯向量搜索，alpha 为 0 是纯文本搜索
+    limit: int   限制数量
+    '''
+    return await knowledge_base_weaviate.search_hybrid(query=query, limit=limit)
+
+
+@router.get("/searchHybrid", description="Query data in weaviate")
+async def search_weaviate_data_by_query(query: str, limit: int, alpha: float = 0.5):
+    """ 查询weaviate的数据,根据query
+    :param query:    查询的字符串
+    :param limit:    限制的数量
+    """
+    return await knowledge_base_weaviate.search_hybrid_or(query=query, alpha=alpha, limit=limit)
 
 
 @router.delete("/deleteData", description="Clear all data in weaviate")
@@ -91,12 +100,3 @@ def search_weaviate_data(uuid: Optional[str] = None, db_id: Optional[str] = None
     else:
         result = knowledge_base_weaviate.search_id_or_database(db_id, database)
     return result
-
-
-@router.get("/searchHybrid", description="Query data in weaviate")
-async def search_weaviate_data_by_query(query: str, limit: int, alpha: float = 0.5):
-    """ 查询weaviate的数据,根据query
-    :param query:    查询的字符串
-    :param limit:    限制的数量
-    """
-    return await knowledge_base_weaviate.search_hybrid_or(query=query, alpha=alpha, limit=limit)
