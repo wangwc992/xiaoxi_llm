96730dde1b9834cac9aeba4f075f684b2c15a50a
wishfyc <wishfyc@163.com>
2024-09-30 10:40:58 +0800

    测试

--- app/data_cleansing/knowledge_base_cleansing.py
+++ app/data_cleansing/knowledge_base_cleansing.py
@@ -931,6 +931,7 @@ def insert_mysql_weaviate(knowledge_base_model_list, uuid_list, file_url_list):
     ai_mysql_weaviate_list = []
     db_name = knowledge_base_model_list[0].get('db_name')
     for i, knowledge_base_model in enumerate(knowledge_base_model_list):
+        logger.info(f"插入mysql_weaviate数据:{knowledge_base_model.get('db_id')}")
         file_url = file_url_list[i] if file_url_list else None
         output = knowledge_base_model.get('output')
         file_content = None
