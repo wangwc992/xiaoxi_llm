d17f5be4ead9630f910733992c9d6e4cf7f898a5
wishfyc <wishfyc@163.com>
2024-10-10 21:23:49 +0800

    测试

--- app/database/mysql/xxlxdb/school/school_major_mapper.py
+++ app/database/mysql/xxlxdb/school/school_major_mapper.py
@@ -10,13 +10,27 @@ def getDepartmentProjectList(intention_study_abroad: IntentionStudyAbroad):
         INNER JOIN zn_school_major_category t3 ON t3.id = t2.category_id
         INNER JOIN zn_school_info t4 ON t4.id = t1.school_id
         INNER JOIN zn_school_rank t5 ON t5.school_id = t4.id
-        WHERE t2.category_id = 1
+        WHERE t2.category_id = 1188
         AND t4.country_name = '澳大利亚' 
         AND t5.world_rank_qs <= 50 
         AND t1.degree_type = '博士' 
         AND t4.english_name = 'UNSW Sydney' 
         AND (FIND_IN_SET('3/年', length_of_schoolings) OR length_of_full = '3.5年')
-        AND opening_month LIKE '%-5,%'
     """
 
-    return xxlxdb.execute_all2dict(sql)
+    return xxlxdb.execute_all2dict(sql ,params=())
+
+
+if __name__ == '__main__':
+    intention_study_abroad = IntentionStudyAbroad(
+        country='澳大利亚',
+        school_en_name='UNSW Sydney',
+        degree='博士',
+        duration='3.5年',
+        semester='5',
+        city='悉尼'
+    )
+
+    x = getDepartmentProjectList(intention_study_abroad)
+
+    print(x)
