53713c3d314b49954e87569efc4ac5495882c26f
wishfyc <wishfyc@163.com>
2024-10-15 11:09:42 +0800

    测试

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '4,5'
+
 import threading
 
 from app.common.core.config import settings
@@ -161,8 +161,8 @@ async def run_server(args, llm_engine=None, **uvicorn_kwargs) -> None:
     server = build_app(args, **uvicorn_kwargs)
 
     # 设置 vLLM 运行在 GPU 1-4
-    # set_vllm_gpus()
-    # await build_server(args, llm_engine)
+    set_vllm_gpus()
+    await build_server(args, llm_engine)
 
     loop = asyncio.get_running_loop()
 
