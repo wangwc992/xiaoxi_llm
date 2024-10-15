77defcaa1710975a21d6dfb1c080da6038b50863
wishfyc <wishfyc@163.com>
2024-10-15 11:03:46 +0800

    测试

--- app/common/core/langchain_client.py
+++ app/common/core/langchain_client.py
@@ -8,7 +8,7 @@ class Embedding:
     """ HuggingFace Embedding,文本向量化  """
     embedding_arg = settings["embedding"]
     # 获取GPU数量,使用最后一个GPU，加载模型，防止内存溢出
-    device_number = '4'
+    device_number = 4
     device = f"cuda:{device_number}" if device_number >= 0 else "cpu"
     model_kwargs = {'device': device}
     encode_kwargs = {'normalize_embeddings': False}
